4d4654c5ff084ca330c64e9a3d999851c3952751
kram <i2395787@icloud.com>
2023-04-11 19:39:45 +0400

    fourth commit

--- backtotop.js
+++ backtotop.js
@@ -1,8 +1,3 @@
-var s = document.createElement("script");
-s.type = "text/javascript";
-s.src = "https://cdn.jsdelivr.net/gh/dmitrykram/Ecwidbacktotop@main/backtotop.js";
-document.head.appendChild(s);
-
 window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {

5e43edf27a41591a906440eb774efbd32ac0fca0
kram <i2395787@icloud.com>
2023-04-11 19:38:11 +0400

    third commit

---
+++ append.js
@@ -0,0 +1,4 @@
+var s = document.createElement("script");
+s.type = "text/javascript";
+s.src = "https://cdn.jsdelivr.net/gh/dmitrykram/Ecwidbacktotop@main/backtotop.js";
+document.head.appendChild(s);
--- backtotop.js
+++ backtotop.js
@@ -1,3 +1,8 @@
+var s = document.createElement("script");
+s.type = "text/javascript";
+s.src = "https://cdn.jsdelivr.net/gh/dmitrykram/Ecwidbacktotop@main/backtotop.js";
+document.head.appendChild(s);
+
 window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {

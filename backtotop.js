b7a52622a1400948c4e32c733b8d703b21a85a13
kram <i2395787@icloud.com>
2023-04-13 21:58:59 +0400

    new commit

--- backtotop.js
+++ backtotop.js
@@ -1,44 +1,4 @@
-var butn = document.createElement("button");
-console.log('Кнопка есть')
-butn.id = "myBtn";
-butn.onclick = "topFunction()";
-
-publicConfig = Ecwid.getAppPublicConfig('custom-app-64446204-2');
-color = JSON.parse(publicConfig);
-console.log(color.btncolor);
-
-color = JSON.parse(publicConfig);
-console.log(color.textvalue);
-
-color = JSON.parse(publicConfig);
-console.log(color.opacityvalue);
-
-color = JSON.parse(publicConfig);
-console.log(color.widthvalue);
-
-color = JSON.parse(publicConfig);
-console.log(color.heightvalue);
-
-color = JSON.parse(publicConfig);
-console.log(color.borderrad);
-
-backbutton = document.getElementById("myBtn");
-backbutton.style.backgroundColor = color.btncolor;
-backbutton.style.textContent = color.textvalue;
-backbutton.style.opacity = color.opacityvalue;
-backbutton.style.width = color.widthvalue;
-backbutton.style.height = color.heightvalue;
-backbutton.style.borderRadius = color.borderrad;
-
-window.onscroll = function() {scrollFunction()};
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("myBtn").style.display = "block";
-    } else {
-        document.getElementById("myBtn").style.display = "none";
-    }
-}
-function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-}
+var s = document.createElement("script");
+s.type = "text/javascript";
+s.src = "https://cdn.jsdelivr.net/gh/dmitrykram/Ecwidbacktotop@main/backtotop.js";
+document.head.appendChild(s);

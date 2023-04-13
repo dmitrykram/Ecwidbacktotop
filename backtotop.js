8b304763ecaf266fa75b17a93ec73bc13b330888
kram <i2395787@icloud.com>
2023-04-13 14:30:15 +0400

    new commit

--- backtotop.js
+++ backtotop.js
@@ -1,3 +1,22 @@
+publicConfig = Ecwid.getAppPublicConfig('my-cool-app');
+color = JSON.parse(publicConfig);
+console.log(color.btncolor);
+
+color = JSON.parse(publicConfig);
+console.log(color.textvalue);
+
+color = JSON.parse(publicConfig);
+console.log(color.opacityvalue);
+
+color = JSON.parse(publicConfig);
+console.log(color.widthvalue);
+
+color = JSON.parse(publicConfig);
+console.log(color.heightvalue);
+
+color = JSON.parse(publicConfig);
+console.log(color.borderrad);
+
 window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {

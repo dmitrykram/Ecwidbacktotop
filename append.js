3c9c068037800a110dea17deca5f51ede4bb9b05
kram <i2395787@icloud.com>
2023-04-14 12:27:17 +0400

    new32 commit

--- append.js
+++ append.js
@@ -1,7 +1,7 @@
 let div = document.createElement('div');
 div.className = "myBtn";
 div.innerHTML = '<button onclick="topFunction()" id="myBtn" title="Back to top"></button>';
-div.style.cssText = 'position: fixed;bottom: -160px;right: -200px;z-index: 99;cursor: pointer;padding: 15px;font-size: 18px;'
+div.style.cssText = 'position: fixed;bottom: 73px;right: -5px;z-index: 99;cursor: pointer;padding: 15px;font-size: 18px;'
 
 document.body.append(div);
 

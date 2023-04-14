ab0a5f88e669be8c8678e9a7394d94e1296195c0
kram <i2395787@icloud.com>
2023-04-14 12:00:21 +0400

    new3 commit

--- append.js
+++ append.js
@@ -5,19 +5,6 @@ div.style.cssText = 'display: none;position: fixed;margin-bottom: 250px;margin-l
 
 document.body.append(div);
 
-window.onscroll = function() {scrollFunction()};
-function scrollFunction() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        document.getElementById("myBtn").style.display = "block";
-    } else {
-        document.getElementById("myBtn").style.display = "none";
-    }
-}
-function topFunction() {
-    document.body.scrollTop = 0;
-    document.documentElement.scrollTop = 0;
-}
-
 backbutton = document.getElementById("myBtn");
 backbutton.style.backgroundColor = color.btncolor;
 backbutton.style.textContent = color.textvalue;
@@ -44,3 +31,16 @@ console.log(color.heightvalue);
 
 color = JSON.parse(publicConfig);
 console.log(color.borderrad);
+
+window.onscroll = function() {scrollFunction()};
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
